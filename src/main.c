60b7a41b7a99982266a1540cfc9fb85f45a20dc9
Evgeniya Sosnovskaya <eva.sosn@yandex.ru>
2020-04-01 16:18:26 +0300

    Подправлено с учетом дополнения  к заданию...

--- src/main.c
+++ src/main.c
@@ -8,23 +8,26 @@
 #define MAX_STACK 16384
 #define MAX_RETURN 16384
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     FILE* input;
     struct forth forth = {0};
     forth_init(&forth, stdin, MAX_DATA, MAX_STACK, MAX_RETURN);
     words_add(&forth);
-    int end = 0;
-    for (int i = 1; i < argc; i++){
+    for (int i = 1; i < argc; i++) {
         input = fopen(argv[i],"r");
         if (input) {
             forth.input = input;
-            forth_run(&forth);
-            end = i;
         }
+        else if (strcmp(argv[i], "-") == 0) {
+            forth.input = stdin;
+        }
+        else {
+            printf("Error: there is no file %s\n", argv[i]);
+            continue;
+        }
+        forth_run(&forth);
     }
-    //printf("%d\n", end);
-    if (argc == 1  || (strcmp(argv[argc - 1], "-") == 0 && (end != argc - 1))) {
+    if (argc == 1) {
         forth.input = stdin;
         forth_run(&forth);
     }
